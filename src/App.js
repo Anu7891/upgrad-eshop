b71e8e9a8f45dbcd3ab0921c187a8684fcbc0897
Anubhav Singh <singhanubhav8783@gmail.com>
2022-11-20 23:52:10 +0530

    Made SignIn Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/Header';
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/* <Header />  */}
+      <SignIn />
     </div>
   );
 }
